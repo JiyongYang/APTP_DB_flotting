7bb8318f67daa355d81e1f61833a77e7525be808
cjh4219 <chj4219@gmail.com>
2017-10-13 18:14:44 +0900

    apply git ignore and resolve conflicts

--- APTP_DB_flotting_project/Program.cs
+++ APTP_DB_flotting_project/Program.cs
@@ -6,85 +6,6 @@ using System.Windows.Forms;
 
 namespace APTP_DB_flotting_project
 {
-    struct ACC
-    {
-        public int user_idx;
-        public double x;
-        public double y;
-        public double z;
-        public DateTime timestamp;
-
-        public ACC(int user_idx, double x, double y, double z, DateTime timestamp)
-        {
-            this.user_idx = user_idx;
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.timestamp = timestamp;
-        }
-    }
-
-    struct BPM
-    {
-        public int user_idx;
-        public int bpm;
-        public DateTime timestamp;
-
-        public BPM(int user_idx, int bpm, DateTime timestamp)
-        {
-            this.user_idx = user_idx;
-            this.bpm = bpm;
-            this.timestamp = timestamp;
-        }
-    }
-
-    struct RRI
-    {
-        public int user_idx;
-        public int rri;
-        public DateTime timestamp;
-
-        public RRI(int user_dix, int rri, DateTime timestamp)
-        {
-            this.user_idx = user_dix;
-            this.rri = rri;
-            this.timestamp = timestamp;
-        }
-    }
-
-    struct USER
-    {
-        public int idx;
-        public string email;
-        public string password;
-        public string name;
-        public int gender;
-        public double height;
-        public double weight;
-        public DateTime signup_time;
-
-        public USER(int idx, string email, string password, string name,
-            int gender, double height, double weight, DateTime signup_time)
-        {
-            this.idx = idx;
-            this.email = email;
-            this.password = password;
-            this.name = name;
-            this.gender = gender;
-            this.height = height;
-            this.weight = weight;
-            this.signup_time = signup_time;
-        }
-    }
-
-    public class MySqlLinkage
-    {
-        List<ACC> list_ACC = new List<ACC>();
-        List<BPM> list_BPM = new List<BPM>();
-        List<RRI> list_RRI = new List<RRI>();
-        List<USER> list_USER = new List<USER>();
-
-    }
 
     static class Program
     {
@@ -99,7 +20,6 @@ namespace APTP_DB_flotting_project
 
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            MySqlLinkage msl = new MySqlLinkage();
             Visualizer form = new Visualizer();
             Application.Run(form);
         }
